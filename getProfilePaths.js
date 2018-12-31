a5dbb80b106f281453cad0b7ae77301152c8a05b
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-12-30 23:22:04 -0500

    Update getProfilePaths.js

--- getProfilePaths.js
+++ getProfilePaths.js
@@ -2,15 +2,20 @@ function checker(elm, type) {  if (elm != undefined) {    if (type == 'src') {  
 
 function reg(elm, n){if(elm != null){return elm[n];}else{return '';}}
 
-var arr = [];
-var d = document;
-var ebcn = (ob,nm)=>{return ob.getElementsByClassName(nm);};
-var ebtn = (ob,nm)=>{return ob.getElementsByTagName(nm);};
+function getProfilePaths(){
+	var arr = [];
+	var d = document;
+	var ebcn = (ob,nm)=>{return ob.getElementsByClassName(nm);};
+	var ebtn = (ob,nm)=>{return ob.getElementsByTagName(nm);};
 
-var alsoViewedCont = ebcn(d,'pv-profile-section__section-info section-info browsemap mt4')[0];
+	var alsoViewedCont = ebcn(d,'pv-profile-section__section-info section-info browsemap mt4')[0];
+	if(alsoViewedCont != null){
+		var alsoViewedList = ebcn(alsoViewedCont, 'pv-browsemap-section__member-container mt4');
 
-var alsoViewedList = ebcn(alsoViewedCont, 'pv-browsemap-section__member-container mt4');
-
-Array.from(alsoViewedList).forEach(itm=>{
-	arr.push(reg(/(?<=in\/).+?(?=\/)/.exec(checker(ebtn(itm, 'a')[0],'href')),0))
-});
+		Array.from(alsoViewedList).forEach(itm=>{
+			arr.push(reg(/(?<=in\/).+?(?=\/)/.exec(checker(ebtn(itm, 'a')[0],'href')),0))
+		});
+    }
+	return arr;
+}
+getProfilePaths()
