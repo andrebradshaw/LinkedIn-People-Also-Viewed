05917b02568e25d6a233a3ea39f1ea547f74febf
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-12-31 07:07:51 -0500

    Update getProfilePaths.js

--- getProfilePaths.js
+++ getProfilePaths.js
@@ -2,20 +2,42 @@ function checker(elm, type) {  if (elm != undefined) {    if (type == 'src') {  
 
 function reg(elm, n){if(elm != null){return elm[n];}else{return '';}}
 
-function getProfilePaths(){
-	var arr = [];
-	var d = document;
-	var ebcn = (ob,nm)=>{return ob.getElementsByClassName(nm);};
-	var ebtn = (ob,nm)=>{return ob.getElementsByTagName(nm);};
+var recordArr = [];
 
-	var alsoViewedCont = ebcn(d,'pv-profile-section__section-info section-info browsemap mt4')[0];
-	if(alsoViewedCont){
-		var alsoViewedList = ebcn(alsoViewedCont, 'pv-browsemap-section__member-container mt4');
+function getProfilePaths() {
+  var arr = [];
+  var d = document;
 
-		Array.from(alsoViewedList).forEach(itm=>{
-			arr.push(reg(/(?<=in\/).+?(?=\/)/.exec(checker(ebtn(itm, 'a')[0],'href')),0))
-		});
+  var pathx = /(?<=in\/).+?(?=\/)/;
+  var entity = reg(pathx.exec(window.location.href), 0);
+
+  var ebcn = (ob, nm) => {
+    return ob.getElementsByClassName(nm)
+  };
+  var ebtn = (ob, nm) => {
+    return ob.getElementsByTagName(nm)
+  };
+
+  var alsoViewedCont = ebcn(d, 'pv-profile-section__section-info section-info browsemap mt4')[0];
+
+  if (alsoViewedCont) {
+    var alsoViewedList = ebcn(alsoViewedCont, 'pv-browsemap-section__member-container mt4');
+
+    Array.from(alsoViewedList).forEach(itm => {
+      arr.push(reg(pathx.exec(checker(ebtn(itm, 'a')[0], 'href')), 0))
+    });
+  }
+  var obj = {
+    'entity_id': entity,
+    'also_viewed': {
+      'timestamp': new Date().getTime(),
+      'entities': arr
     }
-	return arr;
+  }
+  recordArr.push(obj)
+
 }
+
 getProfilePaths()
+recordArr[0].also_viewed.entities
+
